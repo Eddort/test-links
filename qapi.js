819526cd4b367454bf4b1c99f9704404ea387146
Alexander <webisforce@gmail.com>
2020-10-06 16:48:39 +0300

    Some changes

---
+++ qapi.js
@@ -0,0 +1,37 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f7c3c93578742001f2eb006",
+				"5f7c3c93578742001f2eb008",
+				"5f7c3c9c791d77001ead1be7",
+				"5f7c3c9fcf6cfb0022ba8686"
+			]
+		},
+		"5f7c3c93578742001f2eb006": {
+			"id": "5f7c3c93578742001f2eb006",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f7c3c93578742001f2eb008": {
+			"id": "5f7c3c93578742001f2eb008",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"5f7c3c9c791d77001ead1be7": {
+			"id": "5f7c3c9c791d77001ead1be7",
+			"pageUrl": "kek",
+			"name": "kek"
+		},
+		"5f7c3c9fcf6cfb0022ba8686": {
+			"id": "5f7c3c9fcf6cfb0022ba8686",
+			"pageUrl": "kek2",
+			"name": "kek2"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
