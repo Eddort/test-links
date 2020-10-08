2e59bfcfc1eb41e40f7ca19b5e1de47194a2b7c2
Alexander <webisforce@gmail.com>
2020-10-08 14:44:18 +0300

    Some changes

---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as Sadsajs } from "./Sadsajs"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import theme from "theme";
-import { Theme, Text, Link, Button } from "@quarkly/widgets";
+import { Theme, Text, Image, Link, Button } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { Override, Menu, Section } from "@quarkly/components";
+import * as Components from "components";
 export default (() => {
 	return <Theme theme={theme}>
 		<Helmet>
@@ -17,6 +18,7 @@ export default (() => {
 			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
 				home
 			</Text>
+			<Image width="64px" height="64px" src="https://images.unsplash.com/photo-1601758174039-617983b8cdd9?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0MjIzN30&w=2000" />
 			<Menu />
 			<Link href="https://google.com">
 				GOOGLE
@@ -27,11 +29,12 @@ export default (() => {
 			<Text as="p" font="--lead" margin="20px 0 0 0">
 				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
 			</Text>
+			<Components.Sadsajs />
 			<Text as="p" font="--lead" margin="20px 0 0 0">
 				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
 			</Text>
 			<Button font="--lead" margin="20px">
-				Button
+				Buttonыывв
 			</Button>
 		</Section>
 	</Theme>;
